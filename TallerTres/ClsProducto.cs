123621000d154916784a83e36e534fee719e7ef7
Juan Manuel Alvarez Rodriguez <juan.alvarez6@utp.edu.co>
2023-03-27 20:27:58 -0500

    Taller Crear una WebAPI que permita listar una lista de usuarios y de productos

---
+++ TallerTres/ClsProducto.cs
@@ -0,0 +1,13 @@
+﻿namespace TallerTres
+{
+    public class ClsProducto
+    {
+        public string? Nombre { get; set; }
+
+        public String? Codigo {  get; set; }
+
+        public int Precio { get; set; }
+
+        public string? Estado { get; set; }
+    }
+}
---
+++ TallerTres/ClsUsuario.cs
@@ -0,0 +1,16 @@
+﻿namespace TallerTres
+{
+    public class ClsUsuario
+    {
+        public string? Nombre { get; set; }
+
+        public string? Cedula { get; set; }
+
+        public string? Direccion { get; set; }
+
+        public string? Telefono { get; set; }
+
+        public string? FNacimiento { get; set; }
+
+    }
+}
