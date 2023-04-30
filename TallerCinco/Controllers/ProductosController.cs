866a609dbecdd516dee44e1e591788e3fa4ec1d8
Juan Manuel Alvarez Rodriguez <juan.alvarez6@utp.edu.co>
2023-04-30 16:27:48 -0500

    Taller 5 - Base de datos Local

---
+++ TallerCinco/Controllers/ProductosController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TallerCinco.Models;
+
+namespace TallerCinco.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        public readonly WebApiContext _dbcontex;
+
+
+        public ProductosController(WebApiContext _contex)
+        {
+            _dbcontex = _contex;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public IActionResult Listar()
+        {
+
+            List<Producto> listProd = new List<Producto>();
+
+            try
+            {
+                listProd = _dbcontex.Productos.ToList();
+                return StatusCode(StatusCodes.Status200OK, new
+                {
+                    code = "OK",
+                    message = "Consulta Exitosa",
+                    Detail = listProd
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new
+                {
+                    code = "OK",
+                    message = "Error al consultar",
+                    Detail = ex.Message
+                });
+
+            }
+
+        }
+    }
+}
